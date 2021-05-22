69a67a86fcb9fb951037189d40baad4045b4f922
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-22 15:54:49 +0530

    nfc: nq-nci: Compile out unused nfc check functions
    Fixes: f63137680c90e75e4429416e0d119c64e3f8b065 ("nfc: nq-nci: Import cleaned-up xiaomi changes")
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/nfc/nq-nci.c
+++ drivers/nfc/nq-nci.c
@@ -153,6 +153,7 @@ static irqreturn_t nqx_dev_irq_handler(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
+#if 0
 static int is_data_available_for_read(struct nqx_dev *nqx_dev)
 {
 	int ret;
@@ -162,6 +163,7 @@ static int is_data_available_for_read(struct nqx_dev *nqx_dev)
 		!nqx_dev->irq_enabled, msecs_to_jiffies(MAX_IRQ_WAIT_TIME));
 	return ret;
 }
+#endif
 
 static ssize_t nfc_read(struct file *filp, char __user *buf,
 					size_t count, loff_t *offset)
@@ -792,6 +794,7 @@ static const struct file_operations nfc_dev_fops = {
 #endif
 };
 
+#if 0
 /*
  * function: get_nfcc_hw_info()
  *
@@ -1112,6 +1115,7 @@ done:
 
 	return ret;
 }
+#endif
 
 /*
  * Routine to enable clock.
