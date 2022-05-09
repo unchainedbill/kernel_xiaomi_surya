6865e01ec91a4304a6ccb6c478bc4c9cde79263a
taalojarvi <sreedevan05@gmail.com>
2022-05-09 14:31:59 +0530

    drivers: qcacld-3.0: Compile out 'wma_get_status_str'
    warning: unused function 'wma_get_status_str' [-Wunused-function]
    static const char *wma_get_status_str(uint32_t status)
                       ^
    1 warning generated.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

--- drivers/staging/qcacld-3.0/core/wma/src/wma_mgmt.c
+++ drivers/staging/qcacld-3.0/core/wma/src/wma_mgmt.c
@@ -2461,6 +2461,7 @@ void wma_beacon_miss_handler(tp_wma_handle wma, uint32_t vdev_id, int32_t rssi)
  *
  * Return: converted string of tx status
  */
+#ifdef WLAN_DEBUG
 static const char *wma_get_status_str(uint32_t status)
 {
 	switch (status) {
@@ -2473,7 +2474,7 @@ static const char *wma_get_status_str(uint32_t status)
 	CASE_RETURN_STRING(WMI_MGMT_TX_COMP_TYPE_MAX);
 	}
 }
-
+#endif 
 #ifdef CONFIG_HL_SUPPORT
 static inline void wma_mgmt_unmap_buf(tp_wma_handle wma_handle, qdf_nbuf_t buf)
 {
