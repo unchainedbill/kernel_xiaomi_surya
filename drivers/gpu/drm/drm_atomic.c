7186c4ded12b1d9b277278d9f68dec6bc76be118
pri0818 <priyanshusinghal0818@gmail.com>
2022-10-01 15:49:40 +0000

    drm_atomic: Don't boost when kprofiles is disabled
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2258,7 +2258,7 @@ extern int kp_active_mode(void);
 	   * Dont boost CPU & DDR if battery saver profile is enabled
 	   * and boost CPU & DDR if balanced profile is enabled
 	   */
-	  if (kp_active_mode() == 3 || kp_active_mode() == 0) {
+	  if (kp_active_mode() == 3) {
 	    cpu_input_boost_kick_max(25);
 	    devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 75);
 	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 75);
