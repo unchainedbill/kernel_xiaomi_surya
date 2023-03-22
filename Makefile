Stratosphere-Kernel/android_kernel_xiaomi_surya

Makefile: Add RELR to the list of clang-specific options
RELR needs llvm-nm and llvm-objcopy to work, which is not
distributed by GNU binutils. Rather make it a clang-only feature.

Change-Id: I51ab1e932c6fbc51cca38ecef65df1205ddcba27
Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
(cherry picked from commit c8c7e8c2c1522f526297613811c1153d1352c29c)
Signed-off-by: taalojarvi <sreedevan05@gmail.com>

Makefile
@@ -1299,7 +1299,7 @@ endif
 endif
 
 # Disable clang-specific config options when using a different compiler
-clang-specific-configs := LTO_CLANG CFI_CLANG SHADOW_CALL_STACK INIT_STACK_ALL
+clang-specific-configs := LTO_CLANG CFI_CLANG SHADOW_CALL_STACK INIT_STACK_ALL RELR
 
 PHONY += check-clang-specific-options
 check-clang-specific-options: $(KCONFIG_CONFIG) FORCE

unchainedbill/kernel_xiaomi_surya

Makefile
@@ -1306,7 +1306,7 @@ endif
 endif
 
 # Disable clang-specific config options when using a different compiler
-clang-specific-configs := LTO_CLANG CFI_CLANG SHADOW_CALL_STACK INIT_STACK_ALL_ZERO
+clang-specific-configs := LTO_CLANG CFI_CLANG SHADOW_CALL_STACK INIT_STACK_ALL_ZERO RELR
 
 PHONY += check-clang-specific-options
 check-clang-specific-options: $(KCONFIG_CONFIG) FORCE