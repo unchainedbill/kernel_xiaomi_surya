973b3e324a218e995b5e88ec424550f567b7be0f
David112x <86davidx64@gmail.com>
2023-04-17 20:59:38 -0500

    Revert "drm/atomic: Boost CPU & DDR bus according to set kernel profile"
    This reverts commit f77c598786dfbed0e82652840781c10ce0addedd.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -30,9 +30,6 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
-#include <linux/pm_qos.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
 
@@ -2209,8 +2206,6 @@ static void complete_crtc_signaling(struct drm_device *dev,
 	kfree(fence_state);
 }
 
-extern int kp_active_mode(void);
-
 int drm_mode_atomic_ioctl(struct drm_device *dev,
 			  void *data, struct drm_file *file_priv)
 {
@@ -2254,20 +2249,11 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	  /*
-	   * Dont boost CPU & DDR if battery saver profile is enabled
-	   * and boost CPU & DDR if balanced profile is enabled
-	   */
-	  if (kp_active_mode() == 3 || kp_active_mode() == 0) {
-	    cpu_input_boost_kick_max(25);
-	    devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 75);
-	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 75);
-	  } else if (kp_active_mode() == 2) {
-	    cpu_input_boost_kick_max(15);
-	    devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 20);
-	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 20);
-      }
-}
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
+			df_boost_within_input(3250)) {
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
+	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 
